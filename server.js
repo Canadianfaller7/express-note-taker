cb332fa1223736c2741dd0a194cd1ec52dc78a69
Spencer Merrill <canadianfaller7@gmail.com>
2022-12-13 00:34:59 -0700

    added code to server.js to connect my html paths and added gitignore file

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const apiRoutes = require('./routes/index');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
